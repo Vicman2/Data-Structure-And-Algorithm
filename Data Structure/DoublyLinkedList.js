9fb1569b19948f2617e02187485a383041622e88
Offordile Chimaobi Victor <vicmanthebest@gmail.com>
2021-12-16 16:24:27 +0100

    Implemented some methods in the doubly linked list

--- Data Structure/DoublyLinkedList.js
+++ Data Structure/DoublyLinkedList.js
@@ -43,6 +43,65 @@ class DoublyLinkedList{
         this.length--
         return this
     }
+
+    shift(){
+        if(!this.head) return undefined
+
+        let currentHead = this.head
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        }else{
+            let newHead = currentHead.next
+            newHead.prev = null
+            this.head = newHead
+        }
+        this.length--
+        return currentHead
+    }
+    
+    unshift(val){
+        let newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }else{
+            this.head.prev = newNode
+            newNode.next = this.head
+            this.head = newNode
+        }
+
+        this.length++
+        return this
+    }
+
+    get(index){
+        if(index< 0 || index> this.length-1) return null
+
+        let midPoint = Math.floor((this.length -1)/2)
+        let value = null
+        let current 
+        if(index<= midPoint){
+            current = this.head
+            for(let i = 0; i<= midPoint; i++){
+                if(index === i){
+                    value = current
+                    break
+                } 
+                current = current.next
+            }
+        }else{
+            current = this.tail
+            for(let i = this.length -1; i > midPoint; i--){
+                if(index === i){
+                    value = current
+                    break;
+                } 
+                current = current.prev
+            }
+        }
+        return value
+    }
 }
 
 
@@ -51,7 +110,6 @@ let DLL = new DoublyLinkedList()
 DLL.push(23)
 DLL.push(84)
 DLL.push(53)
-DLL.pop()
-DLL.pop()
-DLL.pop()
-console.log(DLL)
+DLL.push(90)
+DLL.push(67)
+console.log(DLL.get(0))
