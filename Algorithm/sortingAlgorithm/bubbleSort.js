c0c8d72f00c11f346bac7c2f0b6774cab57ac69b
Offordile Chimaobi Victor <vicmanthebest@gmail.com>
2022-07-14 08:33:55 +0100

    Refreshed on bubble sort

--- Algorithm/sortingAlgorithm/bubbleSort.js
+++ Algorithm/sortingAlgorithm/bubbleSort.js
@@ -1,34 +1,24 @@
-
-
-
-function bubbleSort(arr){
-    for(let i = 0; i< arr.length; i++){
-        for(let j= 0; j < arr.length;j++){
-            if(arr[j]  > arr[j +1]){
-                let temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1]= temp
-            }
-        }
-    }
-    return arr
+const swap = (arr, index1, index2) => {
+    let temp = arr[index2]
+    arr[index2] = arr[index1]
+    arr[index1] = temp
 }
 
-function bubbleSortModified(arr){
-    let noSwap = true
-    for(let i = arr.length; i >0; i--){
-        for(let j= 0; j < i+1;j++){
-            if(arr[j]  > arr[j +1]){
-                let temp = arr[j]
-                arr[j] = arr[j+1]
-                arr[j+1]= temp
+
+
+const bubbleSort = (arr) => {
+    let noSwap;
+    for(let i = arr.length ; i >=0 ; i--){
+        noSwap = true
+        for(let j =0; j < i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr, j, j+1)
                 noSwap = false
             }
         }
-        if(noSwap) break;
+        if(noSwap) break
     }
     return arr
 }
 
-
-console.log(bubbleSortModified([4,6,2,7,9,2,10,5]))
+console.log(bubbleSort([2,5,3,44,5,6,23,4,2,3,2,71, 1]))
