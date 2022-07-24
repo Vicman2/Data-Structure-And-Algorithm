06138ce0c6a17d9de783b6b24200970994969465
Offordile Chimaobi Victor <vicmanthebest@gmail.com>
2022-07-24 09:42:25 +0100

    Evaluated the selection sort algorithm

--- Algorithm/sortingAlgorithm/selectionSort.js
+++ Algorithm/sortingAlgorithm/selectionSort.js
@@ -5,17 +5,18 @@ const swap = (arr, index1, index2) => {
 }
 
 function selectionSort(arr){
-    for(let i = 0; i< arr.length; i++){
-        let lowest = i
-        for(let j=i+1; j<arr.length; j++){
+    for(let i = 0; i < arr.length; i++){
+        let lowest = i;
+        for(let j = i+1; j< arr.length; j++){
             if(arr[j] < arr[lowest]){
                 lowest = j
             }
         }
         if(i !== lowest){
-            swap(arr, lowest, i)
+            swap(arr, i, lowest)
         }
     }
+
     return arr
 }
 
